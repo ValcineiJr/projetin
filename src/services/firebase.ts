d613852a56de10fc230309120f21d31850be036a
Valcineijr <hakjog@gmail.com>
2022-11-07 17:02:54 -0300

    change firebaseConfig

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -17,12 +17,12 @@ import {
 } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGED_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyB2A_eaXghuX0J7lkSS7UMTDZVK8G_v2aQ",
+  authDomain: "diplomado-30e12.firebaseapp.com",
+  projectId: "diplomado-30e12",
+  storageBucket: "diplomado-30e12.appspot.com",
+  messagingSenderId: "283646397328",
+  appId: "1:283646397328:web:3d9e34b7f3019c48490115",
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
